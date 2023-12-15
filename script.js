bfefb66eb76df371c15b16c65bf7d487fed9eb3a
raul <raullopez.alu@gmail.com>
2023-12-15 18:21:38 +0100

    Commit: Beta UI

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('publicRweet').onclick = ()=>{
+    document.getElementById('cortina').style.display= 'block';
+    document.getElementById('formCont').style.display = 'grid';
+}
+
+document.getElementById('volver').onclick = ()=>{
+    document.getElementById('cortina').style.display= 'none';
+    document.getElementById('formCont').style.display = 'none';
+}
